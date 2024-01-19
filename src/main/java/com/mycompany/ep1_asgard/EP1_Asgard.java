da66c9c7c048b58ec6b914015ff35edaaec83f7f
moises <moises@CHAMBERMIFLACO>
2024-01-18 22:36:02 -0500

    ola

---
+++ src/main/java/com/mycompany/ep1_asgard/EP1_Asgard.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ep1_asgard;
+
+/**
+ *
+ * @author moises
+ */
+public class EP1_Asgard {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
